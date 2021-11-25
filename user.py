2d2b0f566d7f80210ad2a86efe55ada610bc9353
Brandon Schumacher <92617960+btschu@users.noreply.github.com>
2021-11-25 16:50:16 -0700

    added assignment info

--- user.py
+++ user.py
@@ -1,1 +1,13 @@
+class User:
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        self.account_balance = 0
+
+    def make_deposit(self, amount):
+        self.account_balance += amount
+
+    def make_withdrawal(self, amount):
+        self.account_balance -= amount
+
 
