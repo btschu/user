0bf603c8306a23a28373c262f9599500e5cc9f24
Brandon Schumacher <92617960+btschu@users.noreply.github.com>
2021-11-25 19:15:54 -0700

    added instructor's solution for future reference

--- user.py
+++ user.py
@@ -1,4 +1,5 @@
 class User:
+
     def __init__(self, name, email):
         self.name = name
         self.email = email
@@ -7,11 +8,14 @@ class User:
     def make_deposit(self, amount):
         self.account_balance += amount
         return self
+
     def make_withdrawal(self, amount):
         self.account_balance -= amount
         return self
+
     def display_user_balance(self):
         print(self.name, self.account_balance)
+
     def transfer_money(self, other_user, amount):
         self.account_balance -= amount
         other_user.account_balance += amount
@@ -42,3 +46,51 @@ user_three.display_user_balance()
 user_one.transfer_money(user_three, 100)
 user_one.display_user_balance()
 user_three.display_user_balance()
+
+#! Instructor's solution
+# class User:
+
+#     def __init__(self, name):
+#         self.name = name
+#         self.amount = 0
+
+#     def make_deposit(self, amount):
+#         self.amount += amount
+
+#     def make_withdrawl(self,amount):
+#         self.amount -= amount
+
+#     def display_user_balance(self):
+#         print(f"User: {self.name}, Balance: {self.amount}")
+
+#     def transfer_money(self,amount,user):
+#         self.amount -= amount
+#         user.amount += amount
+#         self.display_user_balance()
+#         user.display_user_balance()
+
+
+# adrien = User("Adrien")
+# nibbles = User("Mr. Nibbles")
+# benny_bob = User("Benny Bob")
+
+# adrien.make_deposit(100)
+# adrien.make_deposit(200)
+# adrien.make_deposit(50)
+# adrien.make_withdrawl(45)
+# adrien.display_user_balance()
+
+# nibbles.make_deposit(1000)
+# nibbles.make_deposit(1000)
+# nibbles.make_withdrawl(500)
+# nibbles.make_withdrawl(300)
+# nibbles.display_user_balance()
+
+# benny_bob.make_deposit(1500)
+# benny_bob.make_withdrawl(1000)
+# benny_bob.make_withdrawl(5000)
+# benny_bob.make_withdrawl(3000)
+# benny_bob.display_user_balance()
+
+
+# nibbles.transfer_money(400, adrien)
