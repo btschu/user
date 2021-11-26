291fec7443bfa0db28d2a5473ea1ebdbb21f1ca8
Brandon Schumacher <92617960+btschu@users.noreply.github.com>
2021-11-25 19:24:35 -0700

    fixed f string

--- user.py
+++ user.py
@@ -7,45 +7,42 @@ class User:
 
     def make_deposit(self, amount):
         self.account_balance += amount
-        return self
 
     def make_withdrawal(self, amount):
         self.account_balance -= amount
-        return self
 
     def display_user_balance(self):
-        print(self.name, self.account_balance)
+        print(f"User: {self.name}, Balance: {self.account_balance}")
 
     def transfer_money(self, other_user, amount):
         self.account_balance -= amount
         other_user.account_balance += amount
-        return self
 
-user_one = User("Brandon Schumacher", "brandon@gmail.com")
-user_two = User("Fred Smith", "fred@gmail.com")
-user_three = User("Jill Doe", "jill@gmail.com")
+brandon_schumacher = User("Brandon Schumacher", "brandon@gmail.com")
+fred_smith = User("Fred Smith", "fred@gmail.com")
+jill_doe = User("Jill Doe", "jill@gmail.com")
 
-user_one.make_deposit(1000)
-user_one.make_deposit(500)
-user_one.make_deposit(700)
-user_one.make_withdrawal(500)
-user_one.display_user_balance()
+brandon_schumacher.make_deposit(1000)
+brandon_schumacher.make_deposit(500)
+brandon_schumacher.make_deposit(700)
+brandon_schumacher.make_withdrawal(500)
+brandon_schumacher.display_user_balance()
 
-user_two.make_deposit(500)
-user_two.make_deposit(800)
-user_two.make_withdrawal(300)
-user_two.make_withdrawal(200)
-user_two.display_user_balance()
+fred_smith.make_deposit(500)
+fred_smith.make_deposit(800)
+fred_smith.make_withdrawal(300)
+fred_smith.make_withdrawal(200)
+fred_smith.display_user_balance()
 
-user_three.make_deposit(2500)
-user_three.make_withdrawal(500)
-user_three.make_withdrawal(200)
-user_three.make_withdrawal(400)
-user_three.display_user_balance()
+jill_doe.make_deposit(2500)
+jill_doe.make_withdrawal(500)
+jill_doe.make_withdrawal(200)
+jill_doe.make_withdrawal(400)
+jill_doe.display_user_balance()
 
-user_one.transfer_money(user_three, 100)
-user_one.display_user_balance()
-user_three.display_user_balance()
+brandon_schumacher.transfer_money(jill_doe, 100)
+brandon_schumacher.display_user_balance()
+jill_doe.display_user_balance()
 
 #! Instructor's solution
 # class User:
